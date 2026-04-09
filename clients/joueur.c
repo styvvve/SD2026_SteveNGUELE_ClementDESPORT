64184b40954d3042612b52b9a380ce1b993a1f77
NGUELE BALLA <steve.nguele-balla@etud.univ-pau.fr>
2026-04-09 14:34:13 +0200

    .gitinore and start joueur.c for TCP communication between server and clients (player)

---
+++ clients/joueur.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <string.h>
+
+
+int main(void) {
+    
+}
