36561962a636c8717b1afc2018d359a4ebc40d4b
Desport_Clement <desportclement@gmail.com>
2026-04-09 15:45:34 +0200

    [ADD] - gererJoueur (qui gère les différents joueurs (client)) et dans serveur.c ajout du multi-client TCP

--- Serveur/serveur.c
+++ Serveur/serveur.c
@@ -7,23 +7,22 @@
 #include <netinet/in.h>
 #include <errno.h>
 #include <string.h>
+#include <signal.h>
 #include "socket.c"
+#include "gererJoueur.c"
+
 #define TAILLEBUF 100
 
 
 int main(int argc, char* argv[]) {
     // adresse de la socket locale
-    static struct sockaddr_in addr_local;
+    static struct sockaddr_in addr_serveur_TCP_joueur;
     // adresse de la socket coté serveur (réponse admin)
     static struct sockaddr_in addr_admin;
     // identifiant de l'admin
     struct hostent *host_admin;
-    // taille de l'addresse socket
-    socklen_t lg;
     // descripteur de la socket locale pour l'UDP admin
     int socket_admin;
-    // chaine à envoyer en réponse
-    char *reponse = "bien recu";
     // buffer de réception
     char buffer[TAILLEBUF];
     // chaine reçue
@@ -31,7 +30,64 @@ int main(int argc, char* argv[]) {
     // nombre d'octets lus ou envoyés
     int nb_octets;
 
+    // descripteur de la socket locale pour TCP joueur
+    int socket_ecoute, socket_service;
+
+    // adresse de la socket coté serveur (réponse joueurs)
+    static struct sockaddr_in addr_joueur;
+
+    int lg;
+    // liaison du port pour TCP client
+
+    // port == 2000
+    socket_ecoute = socket(AF_INET, SOCK_STREAM, 0);
+
+    bzero((char *) &addr_serveur_TCP_joueur,sizeof(addr_serveur_TCP_joueur));
+
+
+    addr_serveur_TCP_joueur.sin_family = AF_INET;
+    addr_serveur_TCP_joueur.sin_port = htons(2001);
+
+    if( bind(socket_ecoute,
+        (struct sockaddr*)&addr_serveur_TCP_joueur,sizeof(addr_serveur_TCP_joueur))== -1 ) {
+        perror("erreur bind socket écoute");
+        exit(1);
+    }
+
+
+    if (socket_ecoute == -1){
+        perror("Création de la socket service");
+        exit (1);
+    }
+
+    if (listen(socket_ecoute,0)==-1){
+        perror("Erreur Listen");
+        exit (1);
+    }
+
+    signal(SIGCHLD, SIG_IGN);
+
+
+    int id_joueur=0;
+    while(1){
+        lg = sizeof(struct sockaddr_in);
+        socket_service = accept(socket_ecoute,(struct sockaddr *)&addr_joueur, &lg);
+        ++id_joueur;
+        if (fork()==0){
+            close (socket_ecoute);
+            gererJoueur(socket_service,id_joueur);
+            close(socket_service);
+            exit(0);
+        }
+        close (socket_service);
+    }
+
+
+
     // Crée la socket pour relié l'administrateur et le serveur en UDP pour configuration partie via le port mis en paramètres dans l'execution du prog. (argv[1])
+    
+    
+/*    
     socket_admin = creerSocketUDP_Administrateur(atoi(argv[1]));
 
     // Vérifie si la socket à une erreur
@@ -40,42 +96,17 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    /*ToDo : Relier avec admin et configurer partie par rapport aux "messages" reçu par l'admin*/
-    /*ToDo : Réfléchir à la façon dont l'admin configure la partie (exemple :  1- choisir mode, 2- choisir temps, 3-...)*/
-    /*ToDo : Envoie en temps réelle l'évolution de la partie (score,connexion/deconnexion...)*/
+    ToDo : Relier avec admin et configurer partie par rapport aux "messages" reçu par l'admin
+    ToDo : Réfléchir à la façon dont l'admin configure la partie (exemple :  1- choisir mode, 2- choisir temps, 3-...)
+    ToDo : Envoie en temps réelle l'évolution de la partie (score,connexion/deconnexion...)
+
+
+
     
     close(socket_admin);
 
 
-    /*
-    TP1 pour s'aider
-
-    // attente de données venant d'un client
-    lg = sizeof(struct sockaddr_in);
-    nb_octets = recvfrom(sock, buffer, TAILLEBUF, 0,(struct sockaddr *)&addr_client, &lg);
-    if (nb_octets == -1) {
-        perror("erreur réception paquet");
-        exit(1);
-    }
-    // récupère nom de la machine émettrice des données
-    host_client = gethostbyaddr(&(addr_client.sin_addr), sizeof(long), AF_INET);
-    if (host_client == NULL) {
-        perror("erreur gethostbyaddr");
-        exit(1);
-    }
-    // affichage message reçu et coordonnées émetteur
-    chaine = (char *)malloc(nb_octets * sizeof(char));
-    memcpy(chaine, buffer, nb_octets);
-    printf("recu message %s de la part de %s sur le port %d\n", chaine, host_client->h_name, ntohs(addr_client.sin_port));
-    
-    // envoi de la réponse à l'émetteur
-    nb_octets = sendto(sock, reponse, strlen(reponse)+1, 0,(struct sockaddr*)&addr_client, lg);
-    if (nb_octets == -1) {
-        perror("erreur envoi réponse");
-        exit(1);
-    }
-    // fermeture la socket
-    close(sock); 
-    
     */
+
+
 }
