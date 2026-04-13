99bff17c5f72e87dfecc4c4538fae0c244406989
cdesport001 <desport.clement@etud.univ-pau.fr>
2026-04-13 08:09:34 +0000

    [ADD] - multicast cote client. Quelques bug à corriger - DESPORT

--- clients/joueur.c
+++ clients/joueur.c
@@ -6,6 +6,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <errno.h>
+#include <arpa/inet.h>
 #include <string.h>
 
 
@@ -17,6 +18,52 @@
  * Faire passer nom machine serveur et port distant
  */
 int main(int argc, char *argv[]) {
+
+    /*MULTICAST*/
+
+    // creation socket UDP pour le multicast
+    int sock_udp = socket(AF_INET, SOCK_DGRAM, 0);
+
+    // adresse locale
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(1234);
+
+    // autorisation de réutiliser le port (pour le fonctionnement multicast)
+    int reuse = 1;
+    if (setsockopt(sock_udp, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+        perror("SO_REUSEADDR joueur client");
+        exit(1);
+    }
+
+    if (bind(sock_udp, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        perror("bind multicast joueur ");
+        exit(1);
+    }   
+
+    // abonnement au groupe du multicast
+    struct ip_mreq mreq;
+    mreq.imr_multiaddr.s_addr = inet_addr("226.1.2.3");
+    mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+
+    if (setsockopt(sock_udp, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) {
+        perror("Erreur abonnement multicast");
+        exit(1);
+    }
+
+    if (fork() == 0) {
+        // creation d'une boucle "infini" pour recevoir messages du serveur (multicast)
+        char message[100];
+        while (1) {
+            int n = recvfrom(sock_udp, message, sizeof(message), 0, NULL, 0);
+            if (n > 0) {
+                message[n] = '\0';
+                printf("%s\n", message);
+            }
+        }
+        exit(0);
+    }
 
     static struct sockaddr_in server_addr; 
 
@@ -37,7 +84,7 @@ int main(int argc, char *argv[]) {
         exit(1); 
     } 
 
-    server_host = gethostbyname("scinfe173"); 
+    server_host = gethostbyname("10.0.2.15"); 
     if (server_host == NULL) {
         perror("Error : server adress recovering\n"); 
         exit(1); 
