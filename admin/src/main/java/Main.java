30069f468603281105413c7f335d26e96af75c28
NGUELE BALLA <steve.nguele-balla@etud.univ-pau.fr>
2026-04-29 16:20:26 +0200

    fix -> buffer size in the receiveFromServer() func and fix the Main error in the constructor of Player

--- admin/src/main/java/Main.java
+++ admin/src/main/java/Main.java
@@ -1,10 +1,15 @@
 package main.java;
 
+import main.java.domain.Game;
+import main.java.domain.GameService;
+import main.java.domain.Player;
+import main.java.domain.factory.GameFactory;
 import main.java.infra.connexionUDP;
 
 import java.io.IOException;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -12,23 +17,33 @@ public class Main {
 
         //initialize the connection
         connexionUDP connexion = null; 
-        
-        try {
-            connexion = new connexionUDP("localhost", 3000);
-        } catch (SocketException | UnknownHostException e) {
-            System.out.println("Error during connexion " + e);
-        }
-        try {
-            connexion.sendToServer("test");
-        } catch (IOException e) {
-            System.out.println("Error during sendToServer " + e);
-        }
+        while (true) {
+            try {
+                connexion = new connexionUDP("localhost", 3000);
+            } catch (SocketException | UnknownHostException e) {
+                System.out.println("Error during connexion " + e);
+            }
 
-        try {
-            String chaineRecu = connexion.receiveFromServer();
-            System.out.println(chaineRecu);
-        } catch (IOException e) {
-            System.out.println("Error during receiveFromServer " + e);
+            GameService svr = new GameService();
+            Game newGame = GameFactory.createGame(new ArrayList<>());
+            svr.addGame(newGame);
+            try {
+                connexion.sendToServer("test");
+            } catch (IOException e) {
+                System.out.println("Error during sendToServer " + e);
+            }
+
+            try {
+                String chaineRecu = connexion.receiveFromServer();
+                System.out.println(chaineRecu);
+                String[] commande = chaineRecu.split("\\|");
+                if (commande[0].equals("addPlayer")) {
+                    newGame.addPlayer(new Player(Integer.parseInt(commande[1]), Integer.parseInt(commande[1])));
+                }
+                System.out.println(newGame);
+            } catch (IOException e) {
+                System.out.println("Error during receiveFromServer " + e);
+            }
         }
     }
 }
